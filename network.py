b336622c6f30804f8432d2b2e0611948097c938e
Marijn Roelvink <m.a.t.roelvink@student.tudelft.nl>
2022-02-23 11:22:52 +0100

    Turn python into notebook

--- network.py
+++ network.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 def load_df():
     df = pd.read_excel (r'manufacturing_emails_temporal_network.xlsx')
-    print (df)
+    res = pd.concat([df["node1"], df["node2"]]).unique()
+    print (pd.concat([df["node1"], df["node2"]]))
 
 if __name__ == "__main__":
     load_df()
